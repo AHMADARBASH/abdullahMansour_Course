32bd94426df55e54de1242df23acaf94e10fbded
AHMADARBASH <ahmad.arbash.97@outlook.com>
2022-06-12 00:32:46 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,337 +1,56 @@
+import 'package:bmi/Screens/ArchivedScreen.dart';
+import 'package:bmi/Screens/DoneScreen.dart';
+import 'package:bmi/Screens/ToDoScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const BMICalculator());
+  runApp(ToDoApp());
 }
 
-class BMICalculator extends StatelessWidget {
-  const BMICalculator({Key? key}) : super(key: key);
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: HomePage(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  final Color white = Colors.white;
-  final Color mainColor = const Color.fromARGB(255, 4, 28, 50);
-  final Color secondColor = Colors.redAccent;
-  bool? isMale = true;
-
-  int age = 20;
-  int weight = 50;
-
-  @override
-  void initState() {
-    isMale = null;
-    super.initState();
-  }
-
+class _HomePageState extends State<HomePage> {
+  int _currentIdext = 0;
+  List<String> titles = ['ToDo Tasks', 'Done Tasks', 'New Tasks'];
+  List<Widget> screens = [ToDoScreen(), DoneScreen(), ArchivedScreen()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: mainColor,
-        title: const Text('BMI Calculator'),
-      ),
-      body: Column(children: [
-        Expanded(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                child: GestureDetector(
-                  onTap: () => setState(() {
-                    isMale = true;
-                  }),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          color: isMale == null
-                              ? Colors.transparent
-                              : isMale!
-                                  ? secondColor
-                                  : Colors.transparent,
-                        ),
-                        color: mainColor,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10))),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.male,
-                          color: white,
-                          size: 100,
-                        ),
-                        Text(
-                          'MALE',
-                          style: TextStyle(color: white, fontSize: 25),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+      appBar: AppBar(title: Text(titles[_currentIdext])),
+      body: screens[_currentIdext],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIdext,
+          onTap: (index) {
+            setState(() {
+              _currentIdext = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(label: 'Tasks', icon: Icon(Icons.menu)),
+            BottomNavigationBarItem(
+              label: 'Done',
+              icon: Icon(Icons.check_circle_outline),
             ),
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                child: GestureDetector(
-                  onTap: () => setState(() {
-                    isMale = false;
-                  }),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          color: isMale == null
-                              ? Colors.transparent
-                              : isMale!
-                                  ? Colors.transparent
-                                  : secondColor,
-                        ),
-                        color: mainColor,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10))),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.female,
-                          color: white,
-                          size: 100,
-                        ),
-                        Text(
-                          'FEMALE',
-                          style: TextStyle(color: white, fontSize: 25),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        )),
-        Expanded(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 5),
-          child: CustomHeight(
-            containerColor: mainColor,
-            textColor: white,
-            sliderMainColor: secondColor,
-          ),
-        )),
-        Expanded(
-          child: Row(children: [
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: mainColor,
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(10))),
-                  child: Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 5),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: mainColor,
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(10))),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              'AGE',
-                              style: TextStyle(color: white, fontSize: 25),
-                            ),
-                            Text(
-                              age.toString(),
-                              style: TextStyle(
-                                  color: white,
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                FloatingActionButton(
-                                  backgroundColor: secondColor,
-                                  mini: true,
-                                  onPressed: () {
-                                    if (age == 1) return;
-                                    age--;
-                                    setState(() {});
-                                  },
-                                  child: const Icon(Icons.remove),
-                                ),
-                                FloatingActionButton(
-                                  backgroundColor: secondColor,
-                                  mini: true,
-                                  onPressed: () {
-                                    age++;
-                                    setState(() {});
-                                  },
-                                  child: const Icon(Icons.add),
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: mainColor,
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(10))),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        'WEIGHT',
-                        style: TextStyle(color: white, fontSize: 25),
-                      ),
-                      Text(
-                        weight.toString(),
-                        style: TextStyle(
-                            color: white,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FloatingActionButton(
-                            backgroundColor: secondColor,
-                            mini: true,
-                            onPressed: () {},
-                            child: const Icon(Icons.remove),
-                          ),
-                          FloatingActionButton(
-                            backgroundColor: secondColor,
-                            mini: true,
-                            onPressed: () {},
-                            child: const Icon(Icons.add),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            BottomNavigationBarItem(
+                label: 'Archived', icon: Icon(Icons.archive_outlined)),
           ]),
-        ),
-        MaterialButton(
-          onPressed: () {},
-          color: Colors.redAccent,
-          height: 50,
-          minWidth: double.infinity,
-          child: const Text(
-            'Calculate',
-            style: TextStyle(color: Colors.white),
-          ),
-        )
-      ]),
-    );
-  }
-}
-
-class CustomHeight extends StatefulWidget {
-  const CustomHeight({
-    Key? key,
-    required this.containerColor,
-    required this.textColor,
-    required this.sliderMainColor,
-  }) : super(key: key);
-
-  final Color containerColor;
-  final Color textColor;
-  final Color sliderMainColor;
-  @override
-  State<CustomHeight> createState() => _CustomHeightState();
-}
-
-class _CustomHeightState extends State<CustomHeight> {
-  double height = 120;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: widget.containerColor,
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
-      ),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          Text(
-            'HEIGHT',
-            style: TextStyle(color: widget.textColor, fontSize: 25),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.baseline,
-            textBaseline: TextBaseline.alphabetic,
-            children: [
-              Text(
-                height.toStringAsFixed(0),
-                style: TextStyle(color: widget.textColor, fontSize: 40),
-              ),
-              Text(
-                'CM',
-                style: TextStyle(color: widget.textColor),
-              ),
-            ],
-          ),
-          Slider(
-              activeColor: widget.sliderMainColor,
-              inactiveColor: widget.sliderMainColor.withAlpha(75),
-              value: height,
-              min: 80,
-              max: 220,
-              onChanged: (value) {
-                setState(() {
-                  height = value;
-                });
-              })
-        ],
-      ),
     );
   }
 }
